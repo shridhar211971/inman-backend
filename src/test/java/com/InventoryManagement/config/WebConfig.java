a39fb068c79cf603de0f7efcecd24834c23ef0e9
shridhar211971 <officialshridhar211971@gmail.com>
2025-11-11 12:16:14 +0530

    Update and rename src/test/java/com/InventoryManagement/WebConfig.java to src/test/java/com/InventoryManagement/config/WebConfig.java

--- src/test/java/com/InventoryManagement/config/WebConfig.java
+++ src/test/java/com/InventoryManagement/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.InventoryManagement;
+package com.InventoryManagement.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
