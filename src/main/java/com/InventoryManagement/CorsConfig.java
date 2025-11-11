55d002c168a105e8f737b9676f8bfe06b0f9bc7a
shridhar211971 <officialshridhar211971@gmail.com>
2025-11-11 12:47:00 +0530

    Update CorsConfig.java

--- src/main/java/com/InventoryManagement/CorsConfig.java
+++ src/main/java/com/InventoryManagement/CorsConfig.java
@@ -1,5 +1,5 @@
 package com.InventoryManagement;
-import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,11 +10,13 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200") // Add your Angular app URL
+                .allowedOrigins(
+                        "https://inman-frontend-8gdg.onrender.com", // ✅ Deployed frontend
+                        "http://localhost:4200"                    // ✅ Local dev
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600);
     }
 }
-
